70a14ec1b88110a39c9c4ecdc6212fbad89f05d6
viv-3 <72171270+viv-3@users.noreply.github.com>
2020-11-19 15:09:31 -0800

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,25 @@
+class Ball {
+    constructor(x, y, width, height, angle) {
+      var options = {
+          'restitution':0.8,
+          'frictionAir':0.005,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      Matter.Body.setAngle(this.body,angle)
+      
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos = this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      ellipse( 0, 0, this.width, this.height);
+      pop();
+    }
+}
